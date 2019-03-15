ca198c66fc72d8b526eafcd01d307a65c8097471
Frida <fvp1998@hotmail.com>
2019-03-15 13:41:02 +0100

    2

--- su19-Exercise/Galaga-Exercise-2/Game.cs
+++ su19-Exercise/Galaga-Exercise-2/Game.cs
@@ -319,6 +319,41 @@ namespace Galaga_Exercise_2 {
 
             }
         }
+
+        public void NoMove() {
+            MoveEnemy(null);
+        }
+
+        public void Down(EntityContainer<Enemy> enemies) {
+            MoveEnemies(enemies);
+        }
+
+        public void ZigZagDown(EntityContainer<Enemy> enemies) {
+            float prevPosY = 0.0f;
+
+            foreach (var enem in enemies) {
+                if (((Enemy) enem).shape.Position.Y - prevPosY > 0.1f) {
+                    MoveEnemy((Enemy) enem);
+                    prevPosY = ((Enemy) enem).shape.Position.Y;
+                }
+            }
+        }
+
+    public void MoveEnemy(Enemy enemy) {
+            float newY = 0.0f;
+            float newX = 0.0f;
+
+            newY = enemy.shape.Position.Y - 0.0003f;
+            newX = (float)((0.5f + 0.05f * Math.Sin(2*Math.PI) * (0.9f - newY)/ 0.045f));
+            
+            enemy.shape.Position = new Vec2F(newX, newY);
+        }
+        public void MoveEnemies(EntityContainer<Enemy> enemies) {
+            foreach (var enem in enemies) {
+                ((Enemy) enem).shape.MoveY(-0.005f);
+                
+            }
+        }
     }
 }
  
