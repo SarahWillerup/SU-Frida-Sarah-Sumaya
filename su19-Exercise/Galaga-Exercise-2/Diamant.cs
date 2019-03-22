9c497f815d7397d212b0c2b90e5d26aaa8cf3338
Frida <fvp1998@hotmail.com>
2019-03-22 10:43:56 +0100

    Diamant

--- su19-Exercise/Galaga-Exercise-2/Diamant.cs
+++ su19-Exercise/Galaga-Exercise-2/Diamant.cs
@@ -18,7 +18,7 @@ namespace Galaga_Exercise_2 {
 
         public void CreateEnemies(List<Image> enemyStrides) {
                 Enemies.AddDynamicEntity(new Enemy(
-                    new DynamicShape(new Vec2F(0.1f, 0.9f), new Vec2F(0.1f, 0.1f)),
+                    new DynamicShape(new Vec2F(0.4f, 0.5f), new Vec2F(0.1f, 0.1f)),
                     new ImageStride(80, enemyStrides)));
                 Enemies.AddDynamicEntity(new Enemy(
                     new DynamicShape(new Vec2F(0.2f, 0.7f), new Vec2F(0.1f, 0.1f)),
@@ -36,14 +36,13 @@ namespace Galaga_Exercise_2 {
                     new DynamicShape(new Vec2F(0.6f, 0.7f), new Vec2F(0.1f, 0.1f)),
                     new ImageStride(80, enemyStrides)));
                 Enemies.AddDynamicEntity(new Enemy(
-                    new DynamicShape(new Vec2F(0.7f, 0.9f), new Vec2F(0.1f, 0.1f)),
+                    new DynamicShape(new Vec2F(0.5f, 0.6f), new Vec2F(0.1f, 0.1f)),
                     new ImageStride(80, enemyStrides)));
                 Enemies.AddDynamicEntity(new Enemy(
-                    new DynamicShape(new Vec2F(0.8f, 0.9f), new Vec2F(0.1f, 0.1f)),
+                    new DynamicShape(new Vec2F(0.3f, 0.6f), new Vec2F(0.1f, 0.1f)),
                     new ImageStride(80, enemyStrides)));
     }
   }
 
 }
-   
 
