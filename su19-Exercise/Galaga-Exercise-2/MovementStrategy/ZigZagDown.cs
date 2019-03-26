171b99f2dcb0d7726c7e63ebd134e8a784a311bd
sarahwillerup <willer@live.dk>
2019-03-26 15:26:24 +0100

    2

--- su19-Exercise/Galaga-Exercise-2/MovementStrategy/ZigZagDown.cs
+++ su19-Exercise/Galaga-Exercise-2/MovementStrategy/ZigZagDown.cs
@@ -5,10 +5,36 @@ using Galaga_Exercise_2.GalagaEntities.Enemy;
 
 namespace Galaga_Exercise_2.MovementStrategy {
     
-    public class ZigZagDown : IMovementStrategy {
-        public void MoveEnemy(Enemy enemy) { }
+    public class ZigZagDown {
+        public void MoveEnemy(Enemy enemy) {
+            float newY = 0.0f;
+            float newX = 0.0f;
+
+            newY = enemy.shape.Position.Y - 0.0003f;
+            newX = (float)((0.5f + 0.05f * Math.Sin(2*Math.PI) * (0.9f - newY)/ 0.045f));
+            
+            enemy.shape.Position = new Vec2F(newX, newY);
+        }
 
 
-        public void MoveEnemies(EntityContainer<Enemy> enemies) { }
+        public void MoveEnemies(EntityContainer<Enemy> enemies) {
+            foreach (var enem in enemies) {
+                ((Enemy) enem).shape.MoveY(-0.005f);
+
+            }
+        }
+
+        public void ZigZagD(EntityContainer<Enemy> enemies) {
+            float prevPosY = 0.0f;
+
+            foreach (var enem in enemies) {
+                if (((Enemy) enem).shape.Position.Y - prevPosY > 0.1f) {
+                    MoveEnemy((Enemy) enem);
+                    prevPosY = ((Enemy) enem).shape.Position.Y;
+                }
+            }
+        }
+
+        
     }
 }
